3c218e57238ab5827baeb1ac25186035a07fad2f
Raziya Tabassum <108030074+Raziiy@users.noreply.github.com>
2022-11-24 14:55:42 +0530

    Add files via upload

---
+++ leapYear.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class leapYear{
+
+  public static void main(String[] args) {
+
+Scanner sc=new Scanner(System.in);
+
+      System.out.println(" enter a year.");
+    int year =sc.nextInt();
+    boolean leap = false;
+
+    if (year % 4 == 0) {
+
+     
+      if (year % 100 == 0) {
+
+        if (year % 400 == 0)
+          leap = true;
+        else
+          leap = false;
+      }
+      
+      else
+        leap = true;
+    }
+    
+    else
+      leap = false;
+
+    if (leap)
+      System.out.println(year + " is a leap year.");
+    else
+      System.out.println(year + " is not a leap year.");
+  }
+}
